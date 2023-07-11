01e721d56e1d297f81fa95d1299b1636309d6f20
riva <rivich18@gmail.com>
2023-07-11 20:10:26 +0300

    add email-password auth

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth, provider } from "../firebase-config";
-import { signInWithPopup } from "firebase/auth";
+import { signInWithPopup, createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
 function Login({ setIsAuth }) {
+
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+
+    const [user, setUser] = useState({});
+
+    onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+    });
+
+    const register = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+            console.log(user);
+        } catch (error) {
+            console.log(error.message);
+        }       
+    };
+
+    const login = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+            console.log(user);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    const logout = async () => {
+        await signOut(auth);
+    }
+
     let navigate = useNavigate();
 
     const signInWithGoogle = () => {
@@ -14,10 +48,47 @@ function Login({ setIsAuth }) {
         })
     }
     return (
+        <>
+        <div>
+           <h3> Register User </h3>
+           <input placeholder="Email..." 
+            onChange={(event) => {
+                setRegisterEmail(event.target.value);
+           }}/>
+           <input placeholder="Password..." 
+           onChange={(event) => {
+                setRegisterPassword(event.target.value);
+           }}/>
+
+           <button onClick={register}> Create User </button> 
+        </div>
+
+        <div>
+           <h3> Login </h3>
+           <input placeholder="Email..." 
+           onChange={(event) => {
+                setLoginEmail(event.target.value);
+           }}/>
+           <input placeholder="Password..."
+           onChange={(event) => {
+                setLoginPassword(event.target.value);
+           }}/>
+
+           <button onClick={login}> Login </button> 
+        </div>
+
+        <div>
+        <h4> User Logged In: </h4>
+        {user?.email}
+
+        <button onClick={logout}> Sign Out </button>
+        </div>
+
         <div className="loginPage">
-            <p>Sogn In With Google to Continue</p>
+            <p>Sign In With Google to Continue</p>
             <button className="login-with-google-btn" onClick={signInWithGoogle}>Sign in with Google</button>
         </div>
+        </>
     )
 }
 
